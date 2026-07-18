5f218dbb527f524090f92e0d37a0ba3e542265e5
EnugurthiAnup <eanup.736@gmail.com>
2026-07-18 21:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // complete this js code
-function Person{
+class Person{
 	constructor(name, age) {
 	this.name = name;
 	this.age = age;
 	}
 
 	greet(){
-		 console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+		 console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
 }
 
